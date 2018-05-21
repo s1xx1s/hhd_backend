d307582b4f5aecb2b951e017a52a5ddd84f97045
tomwey <tomwey@163.com>
2018-05-21 17:06:24 +0800

    change some func

--- lib/api/v1/root.rb
+++ lib/api/v1/root.rb
@@ -10,23 +10,25 @@ module API
       before do        
         is_pro = Rails.env.production?
         # 如果访问的是API文档，那么不做判断
-        puts request.path
-        puts request.user_agent
-        
         is_api_doc_path = request.path.include? "swagger_doc"
         encode_str = Base64.urlsafe_encode64(SiteConfig.api_key + params[:i].to_s)
         # (Time.now.to_i - params[:i].to_i) > SiteConfig.access_key_expire_in.to_i
-        
-        # if is_pro && !is_api_doc_path
-#           encode_str = Digest::MD5.hexdigest(SiteConfig.api_key + params[:i].to_s)
-#           if ( (params[:ak].blank? or params[:i].blank?) or encode_str != params[:ak] or $redis.get('ak_code') == params[:i].to_s )
-#             error!({"code" => 403, "message" => "没有访问权限"}, 403)
-#           else
-#             # session[:ak_code] = params[:i].to_s
-#             # 保存上次访问的code
-#             $redis.set('ak_code', params[:i].to_s)
-#           end
-#         end
+        from_api_doc = request.referer && request.referer.include?('/apidoc')
+        # allow_access = request.path.include? "data/mobile"
+        white_lists = []
+        if SiteConfig.api_white_lists
+          white_lists = SiteConfig.api_white_lists.split(',')
+        end
+        if !white_lists.include?(request.path) && !from_api_doc && is_pro && !is_api_doc_path
+          encode_str = Digest::MD5.hexdigest(SiteConfig.api_key + params[:i].to_s)
+          if ( (params[:ak].blank? or params[:i].blank?) or encode_str != params[:ak] or $redis.get('ak_code') == params[:i].to_s )
+            error!({"code" => 403, "message" => "没有访问权限"}, 403)
+          else
+            # session[:ak_code] = params[:i].to_s
+            # 保存上次访问的code
+            $redis.set('ak_code', params[:i].to_s)
+          end
+        end
       end
       
       mount API::V1::UsersAPI
