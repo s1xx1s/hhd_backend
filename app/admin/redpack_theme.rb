fdfa54e54d00eb613682543840720ee5b5808d68
tomwey <tomwey@163.com>
2018-05-21 17:42:42 +0800

    add some func

--- app/admin/redpack_theme.rb
+++ app/admin/redpack_theme.rb
@@ -12,6 +12,23 @@ permit_params :merch_id, :cover, :name, :take_audio, :result_audio, :opened, :so
 #   permitted
 # end
 
+index do
+  selectable_column
+  column '#', :id
+  column 'ID', :uniq_id
+  column :name
+  column '红包封面' do |o|
+    image_tag o.cover.url(:small)
+  end
+  column '所属商家' do |o|
+    o.merchant.try(:name) || '--'
+  end
+  column :opened
+  column 'at', :created_at
+  
+  actions
+end
+
 form html: { multipart: true } do |f|
   f.semantic_errors
   f.inputs '基本信息' do
