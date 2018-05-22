5174c12f433c761687d1d3b99c954616e5bb0873
tomwey <tomwey@163.com>
2018-05-23 00:28:51 +0800

    fixed bug

--- app/controllers/front/application_controller.rb
+++ app/controllers/front/application_controller.rb
@@ -20,6 +20,8 @@ class Front::ApplicationController < ApplicationController
         redirect_to @wx_auth_url
       else
         # puts '不是微信浏览器'
+        url = request.original_url
+        
         redirect_url = "#{SiteConfig.auth_redirect_uri}?url=#{url}&provider=qq"
         # puts redirect_url
         @qq_auth_url = "https://graph.qq.com/oauth2.0/authorize?response_type=code&client_id=#{SiteConfig.qq_app_id}&redirect_uri=#{Rack::Utils.escape(redirect_url)}&scope=get_user_info"
